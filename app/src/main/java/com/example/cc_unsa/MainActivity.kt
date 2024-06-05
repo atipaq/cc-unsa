355252729bed5307692fb606028dd1b7cf29e418
Alex Huaracha <alex.ronny.hqgmail.com>
2024-06-04 19:02:05 -0500

    agregando los fragments y arrglando bugs con el boton del mapa

--- app/src/main/java/com/example/cc_unsa/MainActivity.kt
+++ app/src/main/java/com/example/cc_unsa/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.cc_unsa
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.cc_unsa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+        binding.bottomNavigationView.background = null
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.categories -> replaceFragment(CategoriesFragment())
+                R.id.artists -> replaceFragment(ArtistsFragment())
+                R.id.information -> replaceFragment(InformationFragment())
+            }
+            true
         }
+
+        binding.maps.setOnClickListener {
+            replaceFragment(MapFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
     }
 }
