8a5e21a4512756212ec1985f40dbc4dc5ccfd674
PanoccaJhon <jpanoccam@unsa.edu.pe>
2024-06-27 10:45:09 -0500

    issue: error clases duplicadadas, corregido

---
+++ app/src/main/java/com/example/cc_unsa/activity/HomeActivity.kt
@@ -0,0 +1,28 @@
+package com.example.cc_unsa.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.cc_unsa.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v: View, insets: WindowInsetsCompat ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn_view_map = findViewById<View>(R.id.view_map)
+        btn_view_map.setOnClickListener {
+            val intent = Intent(this, MainMapActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
